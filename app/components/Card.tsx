068f1976c2a4b54b855d134ebbbdcf0dad230457
FredericoVieira <fredops1@hotmail.com>
2024-10-26 12:27:05 -0300

    Add cards grid

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,27 +1,35 @@
-const Card = () => {
+type CardProps = {
+  country: string;
+  flag: string;
+  capital: string;
+  region: string;
+  population: number;
+};
+
+const Card = ({ country, flag, capital, region, population }: CardProps) => {
   return (
     <div className="h-full overflow-hidden bg-white rounded-lg shadow-lg">
       <div className="aspect-video w-full overflow-hidden bg-gray-100">
         <img
-          src="image-url"
-          alt="Flag of Brazil"
+          src={flag}
+          alt={`Flag of ${country}`}
           className="w-full h-full object-cover"
         />
       </div>
       <div className="p-6 text-sm text-gray-600">
-        <h2 className="text-xl font-semibold mb-4">Brazil</h2>
+        <h2 className="text-xl font-semibold mb-4">{country}</h2>
         <div className="space-y-2">
           <div className="flex items-center gap-1">
             <span className="font-semibold">Capital:</span>
-            <span>Brazil</span>
+            <span>{capital}</span>
           </div>
           <div className="flex items-center gap-1">
             <span className="text-sm font-semibold">Region:</span>
-            <span>South America</span>
+            <span>{region}</span>
           </div>
           <div className="flex items-center gap-1">
             <span className="text-sm font-semibold">Population:</span>
-            <span>212600000</span>
+            <span>{population}</span>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,59 @@
 import { Card, Footer, Header } from "./components";
 
+const countries = [
+  {
+    id: 1,
+    country: "Brazil",
+    flag: "/api/placeholder/800/400",
+    capital: "Brasília",
+    region: "South America",
+    population: 214300000,
+  },
+  {
+    id: 2,
+    country: "Japan",
+    flag: "/api/placeholder/800/400",
+    capital: "Tokyo",
+    region: "Asia",
+    population: 125700000,
+  },
+  {
+    id: 3,
+    country: "South Africa",
+    flag: "/api/placeholder/800/400",
+    capital: "Pretoria",
+    region: "Africa",
+    population: 60600000,
+  },
+  {
+    id: 4,
+    country: "Canada",
+    flag: "/api/placeholder/800/400",
+    capital: "Ottawa",
+    region: "North America",
+    population: 38250000,
+  },
+];
+
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <Card />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {countries.map(
+            ({ id, country, flag, capital, region, population }) => (
+              <Card
+                key={id}
+                country={country}
+                flag={flag}
+                capital={capital}
+                region={region}
+                population={population}
+              />
+            )
+          )}
+        </div>
       </main>
       <Footer />
     </>
